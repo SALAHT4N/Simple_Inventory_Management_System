34220bf67e46b0531094d7470b18606a511a4680
salah aldin tanbour <salah.aldin.tanbour@outlook.com>
2023-07-26 20:28:45 +0300

    Initalize repo

---
+++ Simple_Inventory_Management_System/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Simple_Inventory_Management_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
