48ba3f7298c79e90598929db14aa04f780726e8c
salah aldin tanbour <salah.aldin.tanbour@outlook.com>
2023-07-26 20:56:48 +0300

    Implemented Product class properties and methods

---
+++ Simple_Inventory_Management_System/Classes/DisplayFormat.cs
@@ -0,0 +1,7 @@
+﻿namespace Simple_Inventory_Management_System.Classes
+{
+    public enum DisplayFormat
+    {
+        Short, Full
+    }
+}
--- Simple_Inventory_Management_System/Classes/Product.cs
+++ Simple_Inventory_Management_System/Classes/Product.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace Simple_Inventory_Management_System.Classes
 {
-    class Product
+    public class Product
     {
+        public string Name
+        {
+            get; set;
+        } = string.Empty;
+
+        public double Price
+        {
+            get; set;
+        }
+
+        public int Quantity
+        {
+            get; private set;
+        }
+
+        public void IncreaseQuantity(int amount)
+        {
+            Quantity += amount;
+        }
+        public void Consume(int amount)
+        {
+            int amountLeft = Quantity - amount;
+            Quantity = (amountLeft >= 0) ? amountLeft : 0;
+        }
+
+        public string GetDetails(DisplayFormat type) => (type == DisplayFormat.Full) ? FullDisplay() : ShortDisplay();
+      
+        private string ShortDisplay() => $"{Name}: {Quantity} in stock";
+        private string FullDisplay() =>
+            $"Name: {Name}\n" +
+            $"Price: {Price}$\n" +
+            $"Stock: {Quantity}";
     }
 }
