9a635bbff510955994142ba6996682dd5f9700af
salah aldin tanbour <salah.aldin.tanbour@outlook.com>
2023-07-27 17:50:09 +0300

    Create new project
    This commit will add a new project duo to the original one being
    deleted.

---
+++ Inventory_Management_System/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Simple_Inventory_Management_System.Inventory_Management_Library;
+
+namespace Inventory_Management_System
+{
+    public class Program
+    {
+        static private Inventory inventory = new Inventory();
+        static ErrorLevels AddProduct(string name, double price, int quantity)
+        {
+
+        }
+
+        static ErrorLevels DeleteProduct(string name)
+        {
+
+        }
+
+        static ErrorLevels EditProduct(string name, double price, int quantity)
+        {
+
+        }
+
+        static void ListAllProducts()
+        {
+
+        }
+
+        static void SearchForProduct(string name)
+        {
+
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("TEst");
+
+        }
+    }
+}
---
+++ Simple_Inventory_Management_System/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Simple_Inventory_Management_System.Inventory_Management_Library;
+
+namespace Simple_Inventory_Management_System
+{
+    public class Program
+    {
+        static private Inventory inventory = new Inventory();
+        static ErrorLevels AddProduct(string name, double price, int quantity)
+        {
+            
+        }
+
+        static ErrorLevels DeleteProduct(string name)
+        {
+
+        }
+
+        static ErrorLevels EditProduct(string name, double price, int quantity)
+        {
+
+        }
+
+        static void ListAllProducts()
+        {
+
+        }
+
+        static void SearchForProduct(string name)
+        {
+
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("TEst");
+
+        }
+    }    
+}
