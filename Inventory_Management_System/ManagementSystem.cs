66922092eec798aa7d67f3b97afcb4606f3c3f02
salah aldin tanbour <salah.aldin.tanbour@outlook.com>
2023-07-27 21:48:30 +0300

    Move Program.cs functionalities to new class
    This commit will move program.cs methods to a new class managementSystem
    Done to adhere to separation of concerns principle

---
+++ Inventory_Management_System/ManagementSystem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Simple_Inventory_Management_System.Inventory_Management_Library;
+
+namespace Inventory_Management_System
+{
+    public class ManagementSystem
+    {
+        static private Inventory inventory = new Inventory();
+        static ErrorLevels AddProduct(string name, double price, int quantity)
+        {
+
+        }
+
+        static ErrorLevels DeleteProduct(string name)
+        {
+
+        }
+
+        static ErrorLevels EditProduct(string name, double price, int quantity)
+        {
+
+        }
+
+        static void ListAllProducts()
+        {
+
+        }
+
+        static ErrorLevels SearchForProduct(string name)
+        {
+
+        }
+    }
+}
--- Inventory_Management_System/Program.cs
+++ Inventory_Management_System/Program.cs
@@ -5,31 +5,7 @@ namespace Inventory_Management_System
 {
     public class Program
     {
-        static private Inventory inventory = new Inventory();
-        static ErrorLevels AddProduct(string name, double price, int quantity)
-        {
-
-        }
-
-        static ErrorLevels DeleteProduct(string name)
-        {
-
-        }
-
-        static ErrorLevels EditProduct(string name, double price, int quantity)
-        {
-
-        }
-
-        static void ListAllProducts()
-        {
-
-        }
-
-        static void SearchForProduct(string name)
-        {
-
-        }
+        
 
         public static void Main(string[] args)
         {
