5e1a6b627f626adf88089da1b71fb6c1eb478066
salah aldin tanbour <salah.aldin.tanbour@outlook.com>
2023-07-27 13:39:21 +0300

    Introduce Error levels and Incorperate it with Inventory

---
+++ Simple_Inventory_Management_System/Classes/ErrorLevels.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Inventory_Management_System.Classes
+{
+    public enum ErrorLevels
+    {
+        ProductNotFound, ProductAlreadyExists, CannotDo, UnknownCommand, CommandDone, ProductFound
+    }
+}
--- Simple_Inventory_Management_System/Classes/Inventory.cs
+++ Simple_Inventory_Management_System/Classes/Inventory.cs
@@ -6,7 +6,79 @@ using System.Threading.Tasks;
 
 namespace Simple_Inventory_Management_System.Classes
 {
-    class Inventory
+    public class Inventory
     {
+        private List<Product> products = new();
+
+        public Product? GetProduct(string name)
+        {
+            foreach (var p in products)
+            {
+                if (p.Name == name) return p;
+            }
+            return null;
+        }
+        public void AddProduct(string name, double price, int quantity)
+        {
+            Product addedProduct = new() { Name = name, Price = price };
+            products.Add(addedProduct);
+            addedProduct.IncreaseQuantity(quantity);
+        }
+        public ErrorLevels RemoveProduct(string name)
+        {
+            Product remove = GetProduct(name);
+
+            if (remove == null) return ErrorLevels.ProductNotFound;
+
+            products.Remove(remove);
+            return ErrorLevels.CommandDone;
+        }
+
+        private ErrorLevels CheckEditability(string name, string newName)
+        {
+            if (GetProduct(newName) != null) return ErrorLevels.ProductAlreadyExists;
+
+            Product edit = GetProduct(name);
+            if (edit == null) return ErrorLevels.ProductNotFound;
+
+            return ErrorLevels.ProductFound;
+        }
+        
+        public ErrorLevels EditProductQuantity(string name, int quantity)
+        {
+            Product edit = GetProduct(name);
+            if (edit == null) return ErrorLevels.ProductNotFound;
+
+            edit.Consume(edit.Quantity);
+            edit.IncreaseQuantity(quantity);
+
+            return ErrorLevels.CommandDone;
+        }
+        public ErrorLevels EditProductPrice(string name, double price)
+        {
+            Product edit = GetProduct(name);
+            if (edit == null) return ErrorLevels.ProductNotFound;
+            edit.Price = price;
+
+            return ErrorLevels.CommandDone;
+        }
+        public ErrorLevels EditProductName(string name, string newName)
+        {
+            ErrorLevels level = CheckEditability(name, newName);
+            if (level != ErrorLevels.ProductFound) return level;
+
+            Product edit = GetProduct(name);
+            edit.Name = newName;
+
+            return ErrorLevels.ProductFound;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            foreach(var p in products)
+            {
+                yield return p;
+            }
+        }
     }
 }
